41f0bd8a2c57527bd5175c49e39975d31c401745
ChrisBonheur <bonheurmafoundou@gmail.com>
2024-03-17 14:02:19 +0100

    debug to true

--- api_flash/api_flash/settings.py
+++ api_flash/api_flash/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "lOz|u5AhmYTtD:5Ni?tLDW&#VA{fP1%olmB{x}tvCC-]Zq)w"
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', "test-umng-flash.zandosoft.online", "umng-flash.zandosoft.online"]
 
