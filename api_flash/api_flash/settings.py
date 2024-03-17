85f5281fea1b737224d95d16b9ce8174806b7172
ChrisBonheur <bonheurmafoundou@gmail.com>
2024-03-17 08:15:15 +0100

    db

--- api_flash/api_flash/settings.py
+++ api_flash/api_flash/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = "api_flash.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-"""
 
+"""
 DATABASES = {  
     'default': {  
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  
@@ -94,7 +94,7 @@ DATABASES = {
         'PORT': '5432',  
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
